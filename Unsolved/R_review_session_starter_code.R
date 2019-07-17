d664daf6b615893373225339bb5683411e203cd5
Kerry Kurcz <kurczkerry@gmail.com>
2019-07-17 17:44:10 -0500

    starter code initial commit

---
+++ Unsolved/R_review_session_starter_code.R
@@ -0,0 +1,57 @@
+##############
+##### a. printing hello world
+##############
+
+
+
+##############
+##### b. getting and setting working directory
+##############
+
+
+
+##############
+##### c. using built-in data
+##############
+
+# for clean data that's pretty easy to work an experiment with
+data() # this command tells me to do the below command, so ok
+data(package = .packages(all.available = TRUE)) # gives me some warning messages which I ignore
+
+# find yourself an interesting built-in package
+
+# use ?<package-name> to find out more about that package
+
+##############
+##### d. importing data from csv
+##############
+
+# for any csv data your computer can handle
+# hint: use Kaggle or data.gov for reliable data
+
+
+##############
+##### e. installing a library
+##############
+
+##############
+##### f. using a library
+##############
+
+##############
+##### g. for loops
+##############
+
+##############
+##### h. all the applies, what they do, what they mean
+##############
+
+##############
+##### i. decision tree and/or neural network
+##############
+
+##############
+##### j. connecting R to MySQL database
+##############
+
+
